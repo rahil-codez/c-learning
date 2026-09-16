19b066d3a6a4ccacfd9b02453b0e32332ab7f49d
mohd rahil <rahilsh4523y@gmail.com>
2026-09-16 23:57:19 +0530

    added this

---
+++ arithmetic_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a = 5;
+	int b = 3;
+	int c = a+b;
+	printf("the value of a is %d and b is %d and sum is %d\n",a,b,c);
+	printf("the remainder is when a is devided by b is: %d\n", a%b);
+return 0;
+}
