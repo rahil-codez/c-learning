167fe53531cb754b7f5d914e8cd5492aefbd18f6
mohd rahil <rahilsh4523y@gmail.com>
2026-09-16 19:37:11 +0530

    added practice3

---
+++ practice3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+	int main(){
+	int r = 6;
+	int heigth = 10;
+	printf("the area of circle with radius %d is %f",r,3.14*r*r);
+	printf("the the volume of cylinder with radius r %d and heigth %d is %f", r,heigth, 3.14*r*r*heigth);
+return 0;
+}
