98fca60a444c63f40840adadc5de490a68b994fc
mohd rahil <rahilsh4523y@gmail.com>
2026-09-17 09:08:21 +0530

    added file

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+	    printf("Hello, World!\n");
+	printf("hello\t world\n");
+	printf("hello\a world\n");
+	printf("hello\b world\n");
+	printf("lucknow\r EAST\n");
     return 0;
 }
