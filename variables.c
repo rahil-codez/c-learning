8555b4713782e2d5b3c415f7f10ad7375c307ddc
mohd rahil <rahilsh4523y@gmail.com>
2026-09-18 19:32:02 +0530

    added file

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i = 10;
+	int j = i;
+	int a = 2, b= 3, c= 4, d=5;
+	printf("the value of i is %d and the value of j is %d\n",i,j);
+	printf("thr value of a is %d andvalue of b is %d\n",a,b);
+	printf("the value of c is %d and value of d is %d\n",c,d);
+return 0;
+}
