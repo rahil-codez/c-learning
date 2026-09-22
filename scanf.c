d280078cd3f673e1dbff738c161ecc417aba73e9
mohd rahil <rahilsh4523y@gmail.com>
2026-09-22 09:13:22 +0530

    added file

---
+++ scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+	int main(){
+	int x,y,z;
+	printf("enter a number\n");
+	scanf("%d",&x);
+	printf("enter two number\n");
+	scanf("%d%d", &y,&z);
+	int sum = x+y+z;
+	printf("sum =%d\n", sum);
+return 0;
+}
