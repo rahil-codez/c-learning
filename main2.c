08eacad26b9c9dfe475dfada272580892189b224
mohd rahil <rahilsh4523y@gmail.com>
2026-09-22 08:51:28 +0530

    added file

---
+++ main2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+	int main(){
+	float ft = 235.368f;
+	printf("%f\n",ft);
+	printf("%10.2f\n",ft);
+	float ftt = 235.328f;
+	printf("%10.2f\n",ftt);
+	printf("%.2f\n",ftt);
+return 0;
+}
