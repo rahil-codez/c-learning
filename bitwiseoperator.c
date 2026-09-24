44632a3b2df700a0f75ff5f004663a9ca4222af8
mohd rahil <rahilsh4523y@gmail.com>
2026-09-24 22:06:53 +0530

    file added

---
+++ bitwiseoperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+	int main(){
+	unsigned int x = 10;
+	unsigned int y = 5;
+	int a = x&y;
+	printf("a=%d\n",a);
+	int b = x|y;
+	int c = x^y;
+	printf("b=%d\n",b);
+	printf("c=%d\n",c);
+return 0;
+}
+	
