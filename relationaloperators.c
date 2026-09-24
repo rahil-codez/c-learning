764fce43aac1305cc59dfc8f703ccf9b8a5329e6
mohd rahil <rahilsh4523y@gmail.com>
2026-09-24 08:41:47 +0530

    added file

---
+++ relationaloperators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+	int main(){
+	int x = 10, y = 20;
+	printf("the value when x<y=%d\n", x<y);
+	printf("the value whn x>y=%d\n", x>y);
+	printf("the value when x<=y=%d\n", x<=y);
+	printf("the value when x>=y=%d\n", x>=y);
+	printf("the value when x==y=%d\n", x==y);
+	printf("the value when x!=y=%d\n", x!=y);
+return 0;
+}
+
+
+
+
