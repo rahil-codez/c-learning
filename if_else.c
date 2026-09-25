f3bb5be3b2f613adfcda5d8332febf97dbb73277
mohd rahil <rahilsh4523y@gmail.com>
2026-09-25 23:01:12 +0530

    file added

---
+++ if_else.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int age = 5;
+	if(age>10){
+	printf("we are inside if\n");
+	printf("the age is greator then 10\n");
+	}
+	else{
+	printf("your age is not greator then 10\n");
+	}
+return 0;
+} 
